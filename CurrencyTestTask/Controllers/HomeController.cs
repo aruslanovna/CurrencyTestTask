fd697e5a0ddf92eeba357d254d44fd70484668cd
Anastasiia Khrystova <anastasiia.khrystova@convenegroup.com>
2025-02-22 20:27:22 +0100

    Add project files.

---
+++ CurrencyTestTask/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CurrencyExchange.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace CurrencyTestTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HttpClient _httpClient = new HttpClient();
+
+        public HomeController()
+        {
+
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var model = new Currency();
+
+            string json = await _httpClient.GetStringAsync("http://api.currencylayer.com/live?access_key=a3d1c1eb6d68f89ee4bbfe957eeb0a9b");
+            var currencyResponse = JsonConvert.DeserializeObject<CurrencyResponse>(json);
+
+            if (currencyResponse?.Quotes != null)
+            {
+                var currencies = new HashSet<string>();
+
+                foreach (var key in currencyResponse.Quotes.Keys)
+                {
+                    string currencyCode = key.Substring(3);
+                    currencies.Add(currencyCode);
+                }
+
+                model.Currencies = new List<string>(currencies);
+            }
+
+            return View(model);
+        }
+
+        public CurrencyResponse MapToCurrencyResponse(object data)
+        {
+            var currencyResponse = new CurrencyResponse()
+            {
+
+            };
+            return currencyResponse;
+        }
+    }
+}
---
+++ CurrencyTestTask/Models/Currency.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace CurrencyExchange.Models
+{
+    //public class Currency
+    //{
+    //    public decimal OriginalValue { get; set; }
+    //    public decimal? ConvertedValue { get; set; }
+    //    public CurrencyEnum CurrencyFrom { get; set; }
+    //    public CurrencyEnum CurrencyTo { get; set; }
+    //    public decimal ConversionRate { get; set; }
+    //}
+
+    public class Currency
+    {
+        public decimal OriginalValue { get; set; }
+        public string CurrencyFrom { get; set; }
+        public string CurrencyTo { get; set; }
+        public decimal? ConvertedValue { get; set; }
+        public List<string> Currencies { get; set; } = new List<string>();
+    }
+
+
+    public enum CurrencyEnum
+    {
+        USD,
+        EUR,
+        GBP,
+        NOK
+    }
+
+    public class CurrencyResponse
+    {
+        public bool Success { get; set; }
+        public string Terms { get; set; }
+        public string Privacy { get; set; }
+        public long Timestamp { get; set; }
+        public string Source { get; set; }
+        public Dictionary<string, decimal> Quotes { get; set; }
+    }
+}
